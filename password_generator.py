56aae9df875e28acbbf4ef1888cc3eb1131616a8
Anton <vulyy2k@outlook.com>
2016-11-01 23:39:16 -0700

    add the user input

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,12 @@
 import random
 import string
+passwd_len = int(raw_input("Enter the lenght of the password: "))
+
 symbols = "~!@#$%^&*()_|<>?=+-"
 char = string.uppercase + string.lowercase +  symbols + string.digits
 password = " "
 counter = 0
-while counter <=15:
+while counter < passwd_len:
  temp = random.choice(char)
  password = password + temp 
  counter +=1
