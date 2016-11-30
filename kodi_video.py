7e652ce0ca3f9e34f43c0d265deff616f58c00c2
Anton <vulyy2k@outlook.com>
2016-11-30 07:14:58 -0800

    check for javascript

--- kodi_video.py
+++ kodi_video.py
@@ -20,63 +20,74 @@ subvlinks = []
 mList = []
 titleList = []
 linkList = []
+imgList = []
 
 soup = BeautifulSoup(getHTML(url),"html.parser")
 list = soup.find('ul',class_='cfv')
-
+count = 0
 
 for item in list.select('li'):
  mList.append(item.a.get('href'))
- #print (item.a.get('href'))
- #print item.read.strip().encode('utf-8')
  titleList.append(item.a.get('title'))
-# img.append(item.a.get('src'))
+ imgList.append(item.img.get('src'))
 
-for mLink in mList:
- 
- html = getHTML(mLink) 
+#while count < len(mList) - 1:
+#   print mList[count]
+#   print titleList[count]
+#   count = count + 1
+#
+#count = 0
+
+#for mList[count] in mList:
+while count < len(mList) - 1:
+ #print "get video file for ", mList[count],titleList[count]
+ html = getHTML(mList[count]) 
  tempLink = re.compile('<a class="btn-watch" href="(.+?)"').findall(html)
- verify = "javascript"
- confirm = str(tempLink).find(verify) 
- if confirm != -1:
-   mList.remove(mLink)
+ verify = "javascript:alert"
+ confirm = [x for x in tempLink if verify in x] 
+ if len(confirm) == 1:
+   #print 'invalid link', confirm
+   mList.remove(mList[count])
+   del titleList[count]
+   del imgList[count]
    continue
  else:
    html2 = getHTML(tempLink[0])
-   
    vlinks = re.compile('<script type="text/javascript" src="(.+?)"></script>').findall(html2)
    #vlinks = re.compile('a onclick="(.+?)" href="(.+?)"').findall(html2)
    #print vlinks[0] 
    search = "grab."
    result = [s for s in vlinks if search in s]
    if len(result) > 0:
-   #   #subvlinks.append(result)
-   #   print "get the file for ", result
       html3 = getHTML(result[0])
       if html3 == 404:
-   #     print "link broken"
-         mList.remove(mLink)
-         continue
+          #print "link broken", mList[count]
+          
+          #print "remove from mlist", mList[count]
+          #print "remove from title list", titleList[count]
+	  #print "remove from imgList", imgList[count]
+          del mList[count]
+          del titleList[count]
+          del imgList[count]
+          continue
       else:
-        filelink = re.compile('"file":"(.+?)","label"').findall(html3)
-        #if len(filelink)==0:
-        #  link = item
-        if len(filelink)>=1:
-          link  = filelink[len(filelink)-1].replace('\\','').strip('"')
+          filelink = re.compile('"file":"(.+?)","label"').findall(html3)
+          #print "link for mlink", mList[count] 
+	  if len(filelink)==0:
+            link = tempLink[0]
+          if len(filelink)>=1:
+            link  = filelink[len(filelink)-1].replace('\\','').strip('"')
+            linkList.append(link)
+          #print "add video for ", titleList[count]
           #print link
-          linkList.append(link)
    else:
-     mList.remove(mLink)
-     continue     
-print len(mList), len(linkList)
-#for item in subvlinks:
- #html3 = getHTML(item)
- #filelink = re.compile('"file":"(.+?)","label"').findall(html3)
- #if len(filelink)==0:
-  # link = item 
+     del mList[count]
+     continue 
+ count = count + 1    
 
- #if len(filelink)>=1:
- #  link  = filelink[len(filelink)-1].replace('\\','').strip('"')
- #linkList.append(link)
- #print link
+count = 0
 
+while count < len(titleList) - 1:
+   print titleList[count]
+   print linkList[count]
+   count = count + 1
